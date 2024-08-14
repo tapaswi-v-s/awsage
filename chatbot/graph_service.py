b89158acd360f42ee78c0e6d7475c19004b59aea
tapaswi <tapaswisatpanthi@gmail.com>
2024-08-14 18:07:41 -0400

    Update llm prompt

--- chatbot/graph_service.py
+++ chatbot/graph_service.py
@@ -4,6 +4,7 @@ from langchain.tools.retriever import create_retriever_tool
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate, PromptTemplate
+from langchain_core.messages.human import HumanMessage
 from langgraph.prebuilt import tools_condition
 from langchain_core.output_parsers import StrOutputParser
 from langgraph.graph import END, StateGraph, START
@@ -25,7 +26,7 @@ class GraphService:
         pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
         indexes = pc.list_indexes().names()
         
-        if os.environ['PINECONE_INDEX'] in indexes:
+        if os.environ.get('PINECONE_INDEX', 'aws-faq') in indexes:
             return True
         else:
             return False
@@ -35,9 +36,9 @@ class GraphService:
         def init_retriever_tool():
             embeddings = JinaEmbeddings(jina_api_key=os.environ['JINA_API_KEY'], 
                             model_name='jina-embeddings-v2-base-en')
-            vector_store = PineconeVectorStore.from_existing_index(index_name=os.environ['PINECONE_INDEX'],
+            vector_store = PineconeVectorStore.from_existing_index(index_name=os.environ.get('PINECONE_INDEX', 'aws-faq'),
                                                                     embedding=embeddings,
-                                                                    namespace=os.environ['PINECONE_NAMESPACE'], 
+                                                                    namespace=os.environ.get('PINECONE_INDEX', 'FAQ'), 
                                                                     text_key='answer')
             retriever = vector_store.as_retriever(search_kwargs={'k':3})
             return create_retriever_tool(retriever, 'retriever_aws_faqs',
@@ -84,23 +85,19 @@ class GraphService:
             """
 
             messages = state["messages"]
-            question = messages[0].content
+            question = list(filter(lambda x: isinstance(x, HumanMessage), messages))[-1]
             last_message = messages[-1]
-
-            question = messages[0].content
             docs = last_message.content
-            prompt = "You are an assistant for question-answering tasks about amazon web service. \
-                Use the following pieces of retrieved context to answer the question. \
-                If you don't know the answer, just politely say that you don't know. \
-                Keep the answer concise.\nQuestion: {question} \nContext: {context} \nAnswer:"
             
-            prompt = ChatPromptTemplate(
-                input_variables=['context', 'question'],
-                messages=[
-                    HumanMessagePromptTemplate(prompt=
-                                                     PromptTemplate(input_variables=['context', 'question'], 
-                                                                           template=prompt))])
-            if os.environ['USE_FINE_TUNED_MODEL'].lower() == 'true':
+            prompt = ChatPromptTemplate.from_messages([
+                ('system', 'You are a helpful assistant for question-answering tasks about amazon web service.'),
+                ('human', "Use the following pieces of retrieved context to answer the question. \
+                and keep the answer concise. \
+                 If the provided context has no information about the question \
+                 then just reply politely that you don't have the information about it.\
+                \nQuestion: {question} \nContext: {context} \nAnswer:")
+            ])
+            if os.environ.get('USE_FINE_TUNED_MODEL', 'false').lower() == 'true':
                 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
                 model = list(client.fine_tuning.jobs.list())[0].fine_tuned_model
                 llm = ChatOpenAI(model_name=model, temperature=0, streaming=True)
@@ -108,7 +105,7 @@ class GraphService:
                 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
                 
             rag_chain = prompt | llm | StrOutputParser()
-            response = rag_chain.invoke({"context": docs, "question": question})
+            response = rag_chain.invoke({'question':question, 'context':docs})
             return {"messages": [response]}
         self.generate_node = generate_node
 
